e4cbd1419feb41be9a271c730327b041825386cb
Spiro Huang <spiro.huang@derbysoft.com>
2020-03-30 11:18:15 +0800

    fix code style

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/storage/MultipartByteBuffer.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/storage/MultipartByteBuffer.java
@@ -1,152 +1,168 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package io.confluent.connect.s3.storage;
 
 import java.io.ByteArrayInputStream;
-import java.nio.*;
+import java.nio.BufferOverflowException;
 import java.util.LinkedList;
 import java.util.List;
 
-public class MultipartByteBuffer  {
+public class MultipartByteBuffer {
 
-    public static final int MIN_PART_SIZE = 128 * 1024; // 128 KB
-    public static final int INCREMENT_FACT = 4;
-    private final int capacity;
+  public static final int MIN_PART_SIZE = 128 * 1024; // 128 KB
+  public static final int INCREMENT_FACT = 4;
+  private final int capacity;
 
-    private int position;
-    private byte[] currentPart;
-    private int currentPartPos;
+  private int position;
+  private byte[] currentPart;
+  private int currentPartPos;
 
-    private int currentTotalAllocSize;
+  private int currentTotalAllocSize;
 
-    private List<byte[]> multipart = new LinkedList<>();
+  private List<byte[]> multipart = new LinkedList<>();
 
-    private int nextPartSize() {
-        int preNextPartSize = this.currentPart == null ?
-                MIN_PART_SIZE : this.currentPart.length * INCREMENT_FACT;
+  private int nextPartSize() {
+    int preNextPartSize = this.currentPart == null ? MIN_PART_SIZE :
+            this.currentPart.length * INCREMENT_FACT;
 
-        if (this.capacity >= (currentTotalAllocSize + preNextPartSize)) {
-            return preNextPartSize;
-        } else {
-            return this.capacity - this.currentTotalAllocSize;
-        }
+    if (this.capacity >= (currentTotalAllocSize + preNextPartSize)) {
+      return preNextPartSize;
+    } else {
+      return this.capacity - this.currentTotalAllocSize;
+    }
+  }
+
+  private void addNewPart() {
+    int nextPartSize = nextPartSize();
+    if (nextPartSize > 0) {
+      byte[] newPart = new byte[nextPartSize];
+      multipart.add(newPart);
+      currentPart = newPart;
+      currentPartPos = 0;
+      currentTotalAllocSize += nextPartSize;
+    }
+  }
+
+  public MultipartByteBuffer(int capacity) {
+    this.capacity = capacity;
+    this.currentTotalAllocSize = 0;
+    addNewPart();
+    position = 0;
+  }
+
+  public void put(byte b) {
+    if (position >= capacity) {
+      throw new BufferOverflowException();
     }
 
-    private void addNewPart() {
-        int nextPartSize = nextPartSize();
-        if (nextPartSize > 0) {
-            byte[] newPart = new byte[nextPartSize];
-            multipart.add(newPart);
-            currentPart = newPart;
-            currentPartPos = 0;
-            currentTotalAllocSize += nextPartSize;
-        }
+    if (currentPart.length <= currentPartPos) {
+      // current part full
+      addNewPart();
     }
 
-    public MultipartByteBuffer(int capacity) {
-        this.capacity = capacity;
-        this.currentTotalAllocSize = 0;
+    currentPart[currentPartPos] = b;
+    currentPartPos++;
+    position++;
+  }
+
+  public void put(byte[] src, int offset, int length) {
+
+    checkBounds(offset, length, src.length);
+
+    if (length > remaining()) {
+      throw new BufferOverflowException();
+    }
+
+    int remainingOffset = offset;
+    int remainingLen = length;
+    while (remainingLen > 0) {
+      if (currentPart.length <= currentPartPos) {
+        // current part full
         addNewPart();
-        position = 0;
+      }
+      int currentPartRemaining = currentPart.length - currentPartPos;
+      if (currentPartRemaining >= remainingLen) {
+        System.arraycopy(src, remainingOffset, currentPart, currentPartPos, remainingLen);
+        currentPartPos += remainingLen;
+        remainingOffset += remainingLen;
+        remainingLen = 0;
+      } else {
+        System.arraycopy(src, remainingOffset, currentPart, currentPartPos, currentPartRemaining);
+        currentPartPos += currentPartRemaining;
+        remainingOffset += currentPartRemaining;
+        remainingLen -= currentPartRemaining;
+      }
+    }
+    position += length;
+  }
+
+  static void checkBounds(int off, int len, int size) { // package-private
+    if ((off | len | (off + len) | (size - (off + len))) < 0) {
+      throw new IndexOutOfBoundsException();
+    }
+  }
+
+  public boolean hasRemaining() {
+    return capacity > position;
+  }
+
+  public int remaining() {
+    return capacity - position;
+  }
+
+  public int position() {
+    return this.position;
+  }
+
+  public void clear() {
+
+    // remain first part, remove others
+    int size = multipart.size();
+    for (int i = 1; i < size; i++) {
+      multipart.remove(1);
     }
 
-    public void put(byte b) {
-        if (position >= capacity) {
-            throw new BufferOverflowException();
-        }
+    byte[] remain = multipart.get(0);
 
-        if (currentPart.length <= currentPartPos) {
-            // current part full
-            addNewPart();
-        }
+    // reset
+    this.position = 0;
+    this.currentPartPos = 0;
+    this.currentPart = remain;
+    this.currentTotalAllocSize = remain.length;
+  }
 
-        currentPart[currentPartPos] = b;
-        currentPartPos++;
-        position++;
+  public int partNum() {
+    return this.multipart.size();
+  }
+
+  public ByteArrayInputStream getArrayStream() {
+    if (multipart.size() == 1) {
+      return new ByteArrayInputStream(multipart.get(0), 0, this.position);
+    } else {
+      byte[] finalBytes = new byte[this.position];
+      int offset = 0;
+      for (int i = 0; i < (multipart.size() - 1); i++) {
+        byte[] part = multipart.get(i);
+        System.arraycopy(part, 0, finalBytes, offset, part.length);
+        offset += part.length;
+      }
+
+      System.arraycopy(currentPart, 0, finalBytes, offset, currentPartPos);
+      return new ByteArrayInputStream(finalBytes);
     }
-
-    public void put(byte[] src, int offset, int length) {
-
-        checkBounds(offset, length, src.length);
-
-        if (length > remaining()) {
-            throw new BufferOverflowException();
-        }
-
-        int remainingOffset = offset;
-        int remainingLen = length;
-        while (remainingLen > 0) {
-            if (currentPart.length <= currentPartPos) {
-                // current part full
-                addNewPart();
-            }
-            int currentPartRemaining = currentPart.length - currentPartPos;
-            if (currentPartRemaining >= remainingLen) {
-                System.arraycopy(src, remainingOffset, currentPart, currentPartPos, remainingLen);
-                currentPartPos += remainingLen;
-                remainingOffset += remainingLen;
-                remainingLen = 0;
-            } else {
-                System.arraycopy(src, remainingOffset, currentPart, currentPartPos, currentPartRemaining);
-                currentPartPos += currentPartRemaining;
-                remainingOffset += currentPartRemaining;
-                remainingLen -= currentPartRemaining;
-            }
-        }
-        position += length;
-    }
-
-    static void checkBounds(int off, int len, int size) { // package-private
-        if ((off | len | (off + len) | (size - (off + len))) < 0)
-            throw new IndexOutOfBoundsException();
-    }
-
-    public boolean hasRemaining() {
-        return capacity > position;
-    }
-
-    public int remaining() {
-        return capacity - position;
-    }
-
-    public int position() {
-        return this.position;
-    }
-
-    public void clear() {
-
-        // remain first part, remove others
-        int size = multipart.size();
-        for (int i = 1; i < size; i++) {
-            multipart.remove(1);
-        }
-
-        byte[] remain = multipart.get(0);
-
-        // reset
-        this.position = 0;
-        this.currentPartPos = 0;
-        this.currentPart = remain;
-        this.currentTotalAllocSize = remain.length;
-    }
-
-    public int partNum() {
-        return this.multipart.size();
-    }
-
-    public ByteArrayInputStream getArrayStream() {
-        if (multipart.size() == 1) {
-            return new ByteArrayInputStream(multipart.get(0), 0, this.position);
-        } else {
-            byte[] finalBytes = new byte[this.position];
-            int offset = 0;
-            for (int i = 0; i < (multipart.size() - 1); i++) {
-                byte[] part = multipart.get(i);
-                System.arraycopy(part, 0, finalBytes, offset, part.length);
-                offset += part.length;
-            }
-
-            System.arraycopy(currentPart, 0, finalBytes, offset, currentPartPos);
-            return new ByteArrayInputStream(finalBytes);
-        }
-    }
+  }
 
 }
